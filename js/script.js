9abbf1d8f301a83e553b1e3ecb1d1eb8fab1f37d
Benito <bbxblack@gmail.com>
2021-03-23 10:04:38 +0100

    esercizio live

--- js/script.js
+++ js/script.js
@@ -1,7 +1,47 @@
 
+function test() {
 
+  var btns = $('.btn');
+
+  // btns.each(function() {
+  //   console.log($(this));
+  // })
+
+
+
+  // for (var i = 0; i < btns.length; i++) {
+  //
+  //   var btn = $(btns[i]);
+  //   btn.addClass('bg-red')
+  //
+  // }
+
+
+
+
+  // $('.btn').each(function() {
+  //
+  //   console.log($(this));
+  //
+  // });
+
+
+  $('.btn').each(function() {
+
+    var myBtn = $(this)
+
+    // myBtn.pretend('prova');
+    myBtn.html(myBtn.html() = 'prova');
+
+  });
+
+
+
+}
 
 function init() {
+
+  test();
 
 }
 
